b9c10ec2efee023756da3b82b30911f7f884b8c9
Phil Ewels <phil.ewels@scilifelab.se>
2020-04-14 17:06:38 +0200

    Site map: order pages, exclude some

--- wp-content/themes/ngisweden/functions/shortcodes/site_map.php
+++ wp-content/themes/ngisweden/functions/shortcodes/site_map.php
@@ -8,10 +8,23 @@ function ngisweden_site_map_shortcode($atts_raw){
         $post_type = get_post_type_object($pt);
         $html .= '<h3 class="ngisweden_sitemap_posttype_header">'.$post_type->labels->name.'</h3>';
         if(is_post_type_hierarchical($pt)){
+            // Exclude some specific pages
+            $exclude_ids = array();
+            if($pt == 'page'){
+                foreach(['applications', 'bioinformatics', 'news'] as $e_slug){
+                    $e_page = get_page_by_path($e_slug);
+                    if($e_page){
+                        $exclude_ids[] = $e_page->ID;
+                    }
+                }
+            }
+
+            // Get list of page links
             $html .= wp_list_pages(array(
                 'post_type' => $pt,
-                'sort_column' => 'post_title',
+                'sort_column' => 'menu_order',
                 'title_li' => null,
+                'exclude' => implode(',', $exclude_ids),
                 'echo' => false,
                 //////// DEBUG ONLY
                 ///// REMOVE THIS WHEN THE SITE IS GOING LIVE
