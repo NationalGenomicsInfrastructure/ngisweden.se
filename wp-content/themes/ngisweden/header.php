7a2f149388460ba2e9387af2ceb3888a076bd55d
Phil Ewels <phil.ewels@scilifelab.se>
2020-04-18 08:35:53 +0200

    Update global_bootstrap_banner function call name

--- wp-content/themes/ngisweden/header.php
+++ wp-content/themes/ngisweden/header.php
@@ -66,7 +66,7 @@ Check out the theme source on GitHub: https://github.com/nationalGenomicsInfrast
     }
 
     // Customise > Bootstrap Banner
-    if (function_exists('bootstrap_banner')){
-        echo bootstrap_banner();
+    if (function_exists('global_bootstrap_banner')){
+        echo global_bootstrap_banner();
     }
     ?>
