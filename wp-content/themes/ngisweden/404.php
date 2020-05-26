1d36d18ddc99deb1cc852c5d11cb3686eee781bf
Phil Ewels <phil.ewels@scilifelab.se>
2020-05-26 12:38:06 +0200

    404 rewording

--- wp-content/themes/ngisweden/404.php
+++ wp-content/themes/ngisweden/404.php
@@ -3,7 +3,8 @@
 <div class="container main-page">
   <h1>404 - Page Not Found</h1>
   <p>Oh no! We couldn't find <strong><?php echo get_bloginfo('url').$_SERVER['REQUEST_URI']; ?></strong></p>
-  <p>If you found a broken link, please <a href="<?php echo home_url('/contact/'); ?>">let us know where it was</a> so that we can fix it.</p>
+  <p>If you found a broken link please <a href="<?php echo home_url('/contact/'); ?>">get in touch</a>
+    (<a href="mailto:support@ngisweden.se">support@ngisweden.se</a>) and let us know where it was, so that we can fix it.</p>
   <?php echo get_search_form(); ?>
 </div>
 
