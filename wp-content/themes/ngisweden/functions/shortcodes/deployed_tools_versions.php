9ed6e6900ea6826eeb3094e7e7d71efda53952b2
Anandashankar Anil <aanil@users.noreply.github.com>
2022-04-05 15:16:21 +0200

    Update shortcode

--- wp-content/themes/ngisweden/functions/shortcodes/deployed_tools_versions.php
+++ wp-content/themes/ngisweden/functions/shortcodes/deployed_tools_versions.php
@@ -3,55 +3,55 @@
 // NGI Deployed Tools Versions
 // Renders the file generated on the cluster during deployment,
 // showing which versions of which tools are currently in production.
-[ngisweden_deployed_tools_versions_sthlm deployed_file_name='deployed_tools.sthlm.version']
-[ngisweden_deployed_tools_versions_upps deployed_file_name='deployed_tools.upps.version']
 
 function ngisweden_deployed_tools_versions_shortcode($atts_raw){
 
     // Fetch the deployment version file
-    $deployed_file_contents = @file_get_contents(get_template_directory().'/cache/'.$atts_raw['deployed_file_name']);
+    $locations = array('sthlm', 'upps')
+    $output = ''
+    foreach ($locations as &$location){
+      $deployed_file_contents = @file_get_contents(get_template_directory().'/cache/deployed_tools.'.$location.'.version');
 
-    $deployment_type = '';
-    $deployed_at = '';
-    $tools = [];
+      $deployment_type = '';
+      $deployed_at = '';
+      $tools = [];
 
-    foreach(preg_split("/\r\n|\n|\r/", $deployed_file_contents) as $line){
-        if(substr($line, 0, 14) == '-- Deployed at'){
-            $deployed_at = trim(str_replace('--', '', $line));
-        }
-        else if(substr($line, 0, 2) == '--'){
-            $deployment_type = trim(str_replace('--', '', $line));
-        }
-        else {
-            list($tool, $version) = explode(':', $line, 2);
-            if(strlen($tool) > 0){
-                $tools[] = [trim($tool), trim($version)];
-            }
-        }
+      foreach(preg_split("/\r\n|\n|\r/", $deployed_file_contents) as $line){
+          if(substr($line, 0, 14) == '-- Deployed at'){
+              $deployed_at = trim(str_replace('--', '', $line));
+          }
+          else if(substr($line, 0, 2) == '--'){
+              $deployment_type = trim(str_replace('--', '', $line));
+          }
+          else {
+              list($tool, $version) = explode(':', $line, 2);
+              if(strlen($tool) > 0){
+                  $tools[] = [trim($tool), trim($version)];
+              }
+          }
+      }
+
+      if(count($tools) > 0){
+          $output .= '<div class="deployed-tools-versions">';
+          $output .= '<table class="table table-striped table-hover table-sm small">';
+          $output .= '<thead><tr><th>Tool</th><th>Version</th></tr></thead>';
+          $output .= '<tbody>';
+          foreach($tools as $tool){
+              $output .= '<tr><td>'.$tool[0].'</td><td><code>'.$tool[1].'</code></td></tr>';
+          }
+          $output .= '</tbody>';
+          $output .= '</table>';
+          $output .= '<p>Deployment type: '.$deployment_type.'</p>';
+          $output .= '<p>'.$deployed_at.'</p>';
+          $output .= '</div>';
+      }
+      else {
+          $output = '<div class="deployed-tools-versions">';
+          $output .= '<p class="text-muted font-italic">No version information found.</p>';
+          $output .= '</div>';
+      }
     }
-
-    if(count($tools) > 0){
-        $output = '<div class="deployed-tools-versions">';
-        $output .= '<table class="table table-striped table-hover table-sm small">';
-        $output .= '<thead><tr><th>Tool</th><th>Version</th></tr></thead>';
-        $output .= '<tbody>';
-        foreach($tools as $tool){
-            $output .= '<tr><td>'.$tool[0].'</td><td><code>'.$tool[1].'</code></td></tr>';
-        }
-        $output .= '</tbody>';
-        $output .= '</table>';
-        $output .= '<p>Deployment type: '.$deployment_type.'</p>';
-        $output .= '<p>'.$deployed_at.'</p>';
-        $output .= '</div>';
-    }
-    else {
-        $output = '<div class="deployed-tools-versions">';
-        $output .= '<p class="text-muted font-italic">No version information found.</p>';
-        $output .= '</div>';
-    }
-
     return $output;
 
 }
-add_shortcode('ngisweden_deployed_tools_versions_sthlm', 'ngisweden_deployed_tools_versions_shortcode');
-add_shortcode('ngisweden_deployed_tools_versions_upps', 'ngisweden_deployed_tools_versions_shortcode');
+add_shortcode('ngisweden_deployed_tools_versions', 'ngisweden_deployed_tools_versions_shortcode');
