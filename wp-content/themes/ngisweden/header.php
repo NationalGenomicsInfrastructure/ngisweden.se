8157ab6a7cda94730df3c099041653e23fb9bb71
sofia.haglund <sofia.haglund@scilifelab.se>
2024-04-04 14:12:22 +0200

    add google tag to start tracking conversions

--- wp-content/themes/ngisweden/header.php
+++ wp-content/themes/ngisweden/header.php
@@ -19,6 +19,13 @@ Check out the theme source on GitHub: https://github.com/nationalGenomicsInfrast
 
 -->
   <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PZDKQ493');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -31,6 +38,10 @@ Check out the theme source on GitHub: https://github.com/nationalGenomicsInfrast
     <?php wp_head(); ?>
   </head>
   <body <?php body_class('ngisweden'); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZDKQ493"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <nav class="navbar navbar-expand-lg fixed-top navbar-light shadow-sm main-nav-nav" id="main_navbar">
       <div class="container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
